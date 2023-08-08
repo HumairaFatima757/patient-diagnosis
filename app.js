99103bc30ac30e1545c29f407e1acd9cb5e3de6a
HumairaFatima757 <fatimahumaira757@gmail.com>
2023-08-08 09:09:01 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,98 @@
+var a =1 ;
+if (a==1){
+
+
+
+let medi = document.getElementById("medicine")
+medi.onclick = function(){
+    let display = document.getElementById("baradiv").style.display = 'block';
+        return a=0;
+}}else if(a=0){
+    let medi = document.getElementById("medicine")
+medi.onclick = function(){
+    let display = document.getElementById("inp1").style.display = 'none';
+        return a=1;}
+
+}
+
+   
+    let dis1 = document.getElementById("opt1");
+    dis1.onclick=function(){
+      
+     
+        let g =   " use syrup:  cufex   <br> <br>    <button> close </button> "
+        document.getElementById("inp1").style.display = 'block'
+        let f = document.querySelector("#inp1")
+        .innerHTML = g;
+        document.getElementById("baradiv").style.display = 'none'
+
+    }
+
+    let dis2 = document.getElementById("opt2");
+    dis2.onclick=function(){
+      
+        let h =   " use medicine: Acetaminophen  and ibuprofen    <br>    <button> close </button>  "
+        document.getElementById("inp1").style.display = 'block'
+        let i = document.querySelector("#inp1")
+        .innerHTML = h;
+        document.getElementById("baradiv").style.display = 'none'
+    }
+
+    let dis3 = document.getElementById("opt3");
+    dis3.onclick=function(){
+     
+        let h =   " use medicine: panadol and codral    <br>     <button> close </button> "
+        document.getElementById("inp1").style.display = 'block'
+        let i = document.querySelector("#inp1")
+        .innerHTML = h;
+        document.getElementById("baradiv").style.display = 'none'
+    }
+
+    let dis4 = document.getElementById("opt4");
+    dis4.onclick=function(){
+        
+        let h =   " use medicine: crocin and panadol   <br>     <button> close </button>  "
+        document.getElementById("inp1").style.display = 'block'
+   let i = document.querySelector("#inp1")
+   .innerHTML = h;
+   document.getElementById("baradiv").style.display = 'none'
+    }
+
+    let dis5 = document.getElementById("opt5");
+    dis5.onclick=function(){
+ 
+        let h =   " use medicine: Artifen and use movax cream     <br>    <button> close </button> "
+        document.getElementById("inp1").style.display = 'block'
+   let i = document.querySelector("#inp1")
+   .innerHTML = h;
+   document.getElementById("baradiv").style.display = 'none'
+    }
+
+    let dis6 = document.getElementById("opt6");
+    dis6.onclick=function(){
+       
+        let h =   " use medicine: gravinate and serc    <br>     <button> close </button>"
+   let i = document.querySelector("#inp1")
+   .innerHTML = h;
+   document.getElementById("inp1").style.display = 'block'
+   document.getElementById("baradiv").style.display = 'none'
+    }
+    let dis7 = document.getElementById("opt7");
+    dis7.onclick=function(){
+ 
+        let h =   " use medicine: cure for mumps    <br>    <button> close </button> "
+        document.getElementById("inp1").style.display = 'block'
+        let i = document.querySelector("#inp1")
+        .innerHTML = h;
+        document.getElementById("baradiv").style.display = 'none'
+    }
+
+    let inp1 = document.getElementById("inp1")
+    inp1.onclick = function(){
+        let display = document.getElementById("inp1").style.display = 'none';
+    }
+
+
+
+
+
